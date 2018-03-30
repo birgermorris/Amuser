2719e3a8f1f769f9d82ab6de5bb54d67d2bc9384
liesbetvs <r0485110@student.thomasmore.be>
2018-03-30 11:39:50 +0200

    structure

---
+++ register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+</head>
+<body>
+
+<form action="post">
+    <label for="firsname">firstname</label>
+    <input type="text">
+
+    <label for="lastname">lastname</label>
+    <input type="text">
+
+    <label for="password">password</label>
+    <input type="password">
+
+    <button>submit</button>
+</form>
+    
+</body>
+</html>
