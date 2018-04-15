e6baee3d87357ad2b6d2fab786536c1a4b41ac8c
Lisavanpamel <r0654553@student.thomasmore.be>
2018-04-15 20:45:53 +0200

    inloggen (feature 2)
    Functie (inloggen) is nog niet compleet.

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+// Kan ik inloggen?
+function login( $username, $password ){
+    
+    $query = "select * form users where email = '".$conn->real_escape_string($username)."'";
+    $result = $conn->query($query);
+    
+    if($result->num_rows != 1){
+        return false;
+    }
+
+    $user = $_result->fetch_assoc();
+    if( password_verify($password, $user['password'])){
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+?>
