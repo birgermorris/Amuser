55905b7065e433e398e0aca1f838fc852425ead4
Birger <r0657568@student.thomasmore.be>
2018-05-17 15:56:08 +0200

    dbfix

--- classes/db.class.php
+++ classes/db.class.php
@@ -13,7 +13,7 @@ class db
             //self::$conn= new PDO("'mysql:host=".$settings["server"]."; dbname=".$settings["db"]."','".$settings["user"]."','".$settings["pw"]."'");
             //var_dump("'mysql:host=".$settings["server"]."; dbname=".$settings["db"]."','".$settings["user"]."','".$settings["pw"]."'");
             
-            self::$conn= new PDO('mysql:host=localhost; dbname=birgexe198_amuser', 'birgexe198_amuser', 'test1234');
+            self::$conn= new PDO('mysql:host=localhost; dbname=amuser', 'root', '');
             return self::$conn;
         } else {
             return self::$conn;
